fe88e933b423ed9b6ebfe5dc7adc83d767b9fdbb
wujunwei <1399952803@qq.com>
2024-01-30 17:19:08 +0800

    fix douyin parse

--- parser/douyin.go
+++ parser/douyin.go
@@ -15,10 +15,7 @@ type douYin struct{}
 
 func (d douYin) parseVideoID(videoId string) (*VideoParseInfo, error) {
 
-	reqUrl, err := d.getDetailUrlByVideoId(videoId)
-	if err != nil {
-		return nil, err
-	}
+	reqUrl := fmt.Sprintf("https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=%s&a_bogus=64745b2b5bdc4e75b720a9a85b19867a", videoId)
 
 	client := resty.New()
 	res, err := client.R().
@@ -30,17 +27,20 @@ func (d douYin) parseVideoID(videoId string) (*VideoParseInfo, error) {
 		return nil, err
 	}
 
-	data := gjson.GetBytes(res.Body(), "aweme_detail")
+	data := gjson.GetBytes(res.Body(), "item_list.0")
 
 	videoInfo := &VideoParseInfo{
 		Title:    data.Get("desc").String(),
 		VideoUrl: data.Get("video.play_addr.url_list.0").String(),
-		MusicUrl: data.Get("music.play_url.url_list.0").String(),
-		CoverUrl: data.Get("video.origin_cover.url_list.0").String(),
+		MusicUrl: "",
+		CoverUrl: data.Get("video.cover.url_list.0").String(),
 	}
 	videoInfo.Author.Uid = data.Get("author.unique_id").String()
 	videoInfo.Author.Name = data.Get("author.nickname").String()
-	videoInfo.Author.Avatar = data.Get("music.avatar_large.url_list.0").String()
+	videoInfo.Author.Avatar = data.Get("author.avatar_thumb.url_list.0").String()
+
+	// 获取302重定向之后的视频地址
+	d.getRedirectUrl(videoInfo)
 
 	return videoInfo, nil
 }
@@ -66,22 +66,16 @@ func (d douYin) parseShareUrl(shareUrl string) (*VideoParseInfo, error) {
 	return d.parseVideoID(videoId)
 }
 
-func (d douYin) getDetailUrlByVideoId(videoId string) (string, error) {
-	postData := map[string]interface{}{
-		"url":        fmt.Sprintf("https://www.douyin.com/aweme/v1/web/aweme/detail/?aweme_id=%s&aid=1128&version_name=23.5.0&device_platform=android&os_version=2333", videoId),
-		"user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-	}
-
+func (d douYin) getRedirectUrl(videoInfo *VideoParseInfo) {
 	client := resty.New()
-	res, err := client.R().
-		SetHeader(HttpHeaderContentType, "application/json").
-		SetBody(postData).
-		Post("https://tiktok.iculture.cc/X-Bogus")
-	if err != nil {
-		return "", err
+	client.SetRedirectPolicy(resty.NoRedirectPolicy())
+	res2, _ := client.R().
+		SetHeader(HttpHeaderUserAgent, DefaultUserAgent).
+		Get(videoInfo.VideoUrl)
+	locationRes, _ := res2.RawResponse.Location()
+	if locationRes != nil {
+		(*videoInfo).VideoUrl = locationRes.String()
 	}
-
-	return gjson.GetBytes(res.Body(), "param").String(), nil
 }
 
 func (d douYin) randSeq(n int) string {
