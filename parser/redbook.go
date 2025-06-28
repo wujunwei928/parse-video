4e5a58ecdbe5f56641e033f909eb0676f91a0cfc
wujunwei <1399952803@qq.com>
2025-06-28 17:30:09 +0800

    优化小红书分享链接失效时的错误返回

--- parser/redbook.go
+++ parser/redbook.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
-	"strings"
 
 	"github.com/tidwall/gjson"
 
@@ -43,16 +42,7 @@ func (r redBook) parseShareUrl(shareUrl string) (*VideoParseInfo, error) {
 		for _, imageItem := range imagesObjArr {
 			imageUrl := imageItem.Get("urlDefault").String()
 			if len(imageUrl) > 0 {
-				imgId := strings.Split(imageUrl[strings.LastIndex(imageUrl, "/")+1:], "!")[0]
-				// 如果链接中带有 spectrum/ , 替换域名时需要带上
-				spectrumStr := ""
-				if strings.Contains(imageUrl, "spectrum") {
-					spectrumStr = "spectrum/"
-				}
-				newUrl := fmt.Sprintf("https://ci.xiaohongshu.com/%s%s?imageView2/2/w/0/format/jpg", spectrumStr, imgId)
-				fmt.Println(imageUrl)
-				fmt.Println(newUrl)
-				images = append(images, newUrl)
+				images = append(images, imageUrl)
 			}
 		}
 	}
